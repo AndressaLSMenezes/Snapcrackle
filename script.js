d5447f3c831ca3c4ded26d2bcadcdee3337c7be2
Your Correct Name <andressalsmenezes@gmail.com>
2022-06-24 18:12:58 -0300

    Extra e string

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
+function isPrime(num) {
+     for (let i = 2; i < num; i++){
+       if (num % i === 0) {
+         return false
+       }
+      }
+     return true
+   };
+
+
 function snapCrackle(maxValue) {
 
     let newArray = []
 
     for(let i = 1; i <= maxValue; i++) {
 
-        if( i % 2 == 1 && i % 5 != 0) {
+        if( isPrime(i) === true && i > 1) {
+        newArray.push("Prime")
+      } else if( i % 2 == 1 && i % 5 != 0) {
              newArray.push("Snap")
         } else if(i % 5 == 0 && i % 2 == 0) {
              newArray.push("Crackle")
@@ -14,6 +26,5 @@ function snapCrackle(maxValue) {
              newArray.push(i)
         }
     }
-    return newArray
+    return newArray.join([f = ", "])
 }
-console.log(snapCrackle(40))
